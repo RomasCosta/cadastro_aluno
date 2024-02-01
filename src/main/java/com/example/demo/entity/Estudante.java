92e0699f3c68fd0482ef2275a8c9c70a1dee41c8
RomasCosta <romariocosta25@outlook.com>
2024-02-01 09:41:40 -0300

    add estudante repository e alteração versão java 8 pra 11

--- src/main/java/com/example/demo/entity/Estudante.java
+++ src/main/java/com/example/demo/entity/Estudante.java
@@ -5,6 +5,10 @@
 package com.example.demo.entity;
 
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +24,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Estudante {
     
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    
     private String nome;
     private String email;
     private LocalDate dataNascimento;
